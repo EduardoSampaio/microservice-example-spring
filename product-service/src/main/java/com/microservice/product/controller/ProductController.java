219fb9048a3e5820492f125f23697374bcf247ad
Eduardo Sampaio <edwardviana@gmail.com>
2021-12-19 21:41:30 -0300

    first commit

---
+++ product-service/src/main/java/com/microservice/product/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.microservice.product.controller;
+
+import com.microservice.product.domain.Product;
+import com.microservice.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createdProduct(@RequestBody Product product) {
+        productRepository.save(product);
+    }
+}
