37e37174463729c8cc4428a9e3dd950447f76443
Eduardo Sampaio <edwardviana@gmail.com>
2021-12-22 21:53:07 -0300

    inventory service order service ajuste

--- inventory-service/src/main/java/com/microservice/inventory/service/controller/InventoryRestController.java
+++ inventory-service/src/main/java/com/microservice/inventory/service/controller/InventoryRestController.java
@@ -1,4 +1,4 @@
-package com.microservice.inventory.service;
+package com.microservice.inventory.service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
